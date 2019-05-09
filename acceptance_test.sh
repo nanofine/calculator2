f35cbd7c02411764e210ff0b08f0eb056a45d310
jaehyun <jaehyun@email.com>
2019-05-09 15:05:49 +0900

    Add Ip address

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -2,4 +2,5 @@
 test $(curl --silent localhost:8090/sum?a=1\&b=2) -eq 3 || \    # true when it runs on host
 test $(curl --silent $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) -eq 3  # true on docker
 CALCULATOR_PORT=$(docker-compose prot calculator 8090 | cut -d: -f2)
-test $(curl localhost:$CALCULATOR_PORT/sum?a=1\b=2) -eq 3
+echo $CALCULATOR_PORT
+test $(curl --silent 210.125.29.97:$CALCULATOR_PORT/sum?a=1\b=2) -eq 3
